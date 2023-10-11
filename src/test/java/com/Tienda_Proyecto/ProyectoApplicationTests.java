02394c6fba71abdf3337ef0560fe10511c32ebc4
Katerine18 <ycubero10366@ufide.ac.cr>
2023-10-11 16:10:08 -0600

    ACTUALIZACION CON TEMPLATES

--- src/test/java/com/Tienda_Proyecto/ProyectoApplicationTests.java
+++ src/test/java/com/Tienda_Proyecto/ProyectoApplicationTests.java
@@ -1,7 +1,8 @@
 package com.Tienda_Proyecto;
 
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;;
 
 @SpringBootTest
 class ProyectoApplicationTests {
