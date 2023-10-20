afb9c0396acaa85681f19826b7bd3865763f6d8a
DKnexin <denis421998@gmail.com>
2023-10-19 19:58:32 -0600

    Denis Mejia - Commit

---
+++ src/test/java/com/Tienda_Proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Tienda_Proyecto;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
